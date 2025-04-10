b94e888aec2ceb97a69505e546ad4d136bc00680
isaac <isaac.txuj23@gmail.com>
2025-04-10 00:19:31 +0000

    good stuff

--- stuff.js
+++ stuff.js
@@ -5,6 +5,14 @@ const songs = [
     "Knee Socks",
 ];
 
+// Dictionary Example
+// const songsDict = {
+//     "The Car": "<iframe width="784" height="441" src="https://www.youtube.com/embed/hWoG5XAOPvY" title="Arctic Monkeys - The Car (Official Audio)" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>",
+// }
+
+// video = "www.youtube.com/watch?" + songTitle + ".com"
+// <iframe width="784" height="441" src="https://www.youtube.com/embed/hWoG5XAOPvY" title="Arctic Monkeys - The Car (Official Audio)" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
 // Defines the button and display in HMTL
 const button = document.getElementById("songButton");
 const display = document.getElementById("songTitle");
